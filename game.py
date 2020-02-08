040f503e426934441709d6a1ef3ea1bef91b3d8f
harshkanjariya <harshgkanjariya2468@gmail.com>
2020-02-08 21:51:33 +0530

    graphics

--- game.py
+++ game.py
@@ -1,86 +1,137 @@
-from pygame import *
-init()
-screen=display.set_mode([1200,600])
+from datetime import datetime
+import pytz
+import pygame
+pygame.mixer.pre_init(44100, 16, 2, 4096)
+pygame.init()
+screen=pygame.display.set_mode([1200,600])
 width,height=screen.get_size()
 running=True
 pressed=False
 flip=False
 colliding=False
-clock=time.Clock()
-all_blocks=sprite.Group()
+coinsize=30
+bricksize=60
+clock=pygame.time.Clock()
+font=pygame.font.Font(None,30)
+pygame.mixer.init()
+coingain=pygame.mixer.Sound("coin.wav")
+all_blocks=pygame.sprite.Group()
 camera=[width/2,height/2]
 
-class Player(sprite.Sprite):
-	def __init__(self,color,width,height):
+class Player(pygame.sprite.Sprite):
+	def __init__(self,name,color,width,height):
 		super().__init__()
-		self.image=Surface([width,height])
+		self.image=pygame.Surface([width,height])
 		self.image.fill(color)
 		self.image.set_colorkey(color)
 		self.color=color
+		self.name=name
 		self.rect=self.image.get_rect()
 		self.vx=0
 		self.vy=0
 	def update(self):
 		self.rect.x+=self.vx
 		self.rect.y+=self.vy
-
-class Wall(sprite.Sprite):
-	def __init__(self,color,width,height):
+class Other(pygame.sprite.Sprite):
+	def __init__(self,name,color,x,y,coinsize):
 		super().__init__()
-		self.image=Surface([width,height])
+		self.image=pygame.Surface([coinsize,coinsize])
 		self.image.fill(color)
 		self.image.set_colorkey(color)
 		self.color=color
 		self.rect=self.image.get_rect()
+		self.rect.x=x
+		self.rect.y=y
+		self.count=0
+		self.name=name
+		self.visible=True
+	def update(self):
+		self.count=(self.count+2)%60
 	def show(self):
-		global screen,camera
-		draw.rect(screen,self.color,[self.rect.x-camera[0],self.rect.y-camera[1],self.rect.width,self.rect.height])
+		if self.visible:
+			global screen,camera
+			screen.blit(coins[int(self.count/12)],(self.rect.x-camera[0],self.rect.y-camera[1],50,50))
+class Wall(pygame.sprite.Sprite):
+	def __init__(self,color,x,y,blocks,bricksize):
+		super().__init__()
+		self.image=pygame.Surface([bricksize*blocks,bricksize])
+		self.image.fill(color)
+		self.image.set_colorkey(color)
+		self.color=color
+		self.rect=self.image.get_rect()
+		self.rect.x=x
+		self.rect.y=y
+		self.blocks=blocks
+	def show(self):
+		global screen,camera,bricksize,brickimg
+		for i in range(self.blocks):
+			screen.blit(brickimg,(self.rect.x+i*bricksize-camera[0],self.rect.y-5-camera[1],bricksize,bricksize+5))
+		# pygame.draw.rect(screen,self.color,[self.rect.x-camera[0],self.rect.y-camera[1],self.rect.width,self.rect.height])
 
-player=Player((0,0,0),70,100)
+player=Player("main",(0,0,0),70,100)
 player.rect.x=20
 player.rect.y=20
 all_blocks.add(player)
-idle=image.load('sprite0.png')
-idle=transform.scale(idle,(player.rect.width,player.rect.height))
+idle=pygame.image.load('character/sprite0.png')
+idle=pygame.transform.scale(idle,(player.rect.width,player.rect.height))
 boy=[]
 imgcount=0
 for i in range(10):
-	im=image.load('sprite'+str(i+1)+'.png')
-	im=transform.scale(im,(player.rect.width,player.rect.height))
+	im=pygame.image.load('character/sprite'+str(i+1)+'.png')
+	im=pygame.transform.scale(im,(player.rect.width,player.rect.height))
 	boy.append(im)
 jump=[]
 for i in range(7):
-	im=image.load('jump'+str(i+1)+'.png')
-	im=transform.scale(im,(player.rect.width,player.rect.height))
+	im=pygame.image.load('character/jump'+str(i+1)+'.png')
+	im=pygame.transform.scale(im,(player.rect.width,player.rect.height))
 	jump.append(im)
+coins=[]
+coin=Other("coin 1",(0,0,0),1035,350,coinsize)
+all_blocks.add(coin)
+coin=Other("coin 2",(0,0,0),1215,150,colliding)
+all_blocks.add(coin)
+for i in range(5):
+	im=pygame.image.load('coins/'+str(i)+'.png')
+	im=pygame.transform.scale(im,(coinsize,coinsize))
+	coins.append(im)
 
-block=Wall((0,255,0),500,50)
-block.rect.x=20
-block.rect.y=500
+flowerimg=pygame.image.load('flower.png')
+flowerimg=pygame.transform.scale(flowerimg,(30,30))
+flowerimgrect=flowerimg.get_rect()
+flowerimgrect.x=720
+flowerimgrect.y=70
+brickimg=pygame.image.load('brick.png')
+brickimg=pygame.transform.scale(brickimg,(bricksize,bricksize))
+
+block=Wall((0,255,0),20,500,8,bricksize)
 all_blocks.add(block)
-block=Wall((0,255,0),500,50)
-block.rect.x=600
-block.rect.y=400
+block=Wall((0,255,0),600,400,8,bricksize)
+all_blocks.add(block)
+block=Wall((0,255,0),1200,200,1,bricksize)
+all_blocks.add(block)
+block=Wall((0,255,0),1700,200,1,bricksize)
+all_blocks.add(block)
+block=Wall((0,255,0),700,100,1,bricksize)
 all_blocks.add(block)
 
 a=0.1
 friction=0.6
 def face(im):
 	if flip:
-		return transform.flip(im,True,False)
+		return pygame.transform.flip(im,True,False)
 	else:
 		return im
 def playerpos():
 	return (player.rect.x-camera[0],player.rect.y-camera[1],player.rect.width,player.rect.height)
 def collisions():
-	collide_list=sprite.spritecollide(player,all_blocks,False)
+	collide_list=pygame.sprite.spritecollide(player,all_blocks,False)
 	global colliding
 	colliding=False
 	for b in collide_list:
-		if not b==player:
-			colliding=True
+		if isinstance(b,Wall):
 			if player.rect.x+player.rect.width-5>b.rect.x and player.rect.x+5<b.rect.x+b.rect.width:
 				if player.rect.y+player.rect.height/2<b.rect.y+b.rect.height/2 and player.vy>0:
+					colliding=True
 					player.vy=0
 					player.rect.y=b.rect.y-player.rect.height+1
 					if not pressed:
@@ -101,27 +152,36 @@ def collisions():
 				elif player.rect.x>b.rect.x:
 					player.vx=1
 					player.rect.x=b.rect.x+b.rect.width
+		elif isinstance(b,Other):
+			b.visible=False
+			all_blocks.remove(b)
+			del b
+			coingain.play()
 while running:
-	for e in event.get():
-		if e.type == QUIT:
+	for e in pygame.event.get():
+		if e.type == pygame.QUIT:
 			running=False
-		if e.type == KEYDOWN:
-			if e.key == K_RIGHT:
+		if e.type == pygame.KEYDOWN:
+			if e.key == pygame.K_RIGHT:
 				pressed=True
 				player.vx=5
 				flip=False
-			elif e.key == K_LEFT:
+			elif e.key == pygame.K_LEFT:
 				pressed=True
 				flip=True
 				player.vx=-5
-			elif e.key == K_UP:
-				player.vy=-5
-		elif e.type == KEYUP:
-			if e.key == K_RIGHT:
+			elif e.key == pygame.K_UP:
+				if colliding:
+					player.vy=-5
+			elif e.key == pygame.K_SPACE:
+				if colliding:
+					player.vy=-6
+		elif e.type == pygame.KEYUP:
+			if e.key == pygame.K_RIGHT:
 				pressed=False
-			elif e.key == K_LEFT:
+			elif e.key == pygame.K_LEFT:
 				pressed=False
-	screen.fill((255,255,255))
+	screen.fill((100,255,255))
 	player.vy+=a
 	if player.rect.x-camera[0]<width/4:
 		camera[0]=player.rect.x-width/4
@@ -132,9 +192,13 @@ while running:
 	elif player.rect.y-camera[1]<height/4:
 		camera[1]=player.rect.y-height/4
 	collisions()
+	screen.blit(flowerimg,(flowerimgrect.x-camera[0],flowerimgrect.y-camera[1],flowerimgrect.width,flowerimgrect.height))
 	player.update()
 	for block in all_blocks:
-		if not block==player:
+		if isinstance(block,Wall):
+			block.show()
+		elif isinstance(block,Other):
+			block.update()
 			block.show()
 	if not colliding:
 		if player.vy<-0.5:
@@ -149,6 +213,9 @@ while running:
 	else:
 		imgcount=(imgcount+1.5)%60
 		screen.blit(face(boy[int(imgcount/6)]),playerpos())
-	display.update()
+	text=font.render('Date and Time : '+datetime.utcnow().strftime("%d/%m/%Y %H:%M:%S"),1,(0,0,0))
+	textpos=text.get_rect(centerx=screen.get_width()/2)
+	screen.blit(text,textpos)
+	pygame.display.update()
 	clock.tick(60)
 quit()
