6a21e613406c72c9b6829a52df7abbe037c16509
harshkanjariya <harshgkanjariya2468@gmail.com>
2020-02-15 11:43:44 +0530

    change

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import os
 import pytz
 import pygame
+import json
 pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.init()
 screen=pygame.display.set_mode([1200,600])
@@ -163,9 +164,12 @@ class Wall(pygame.sprite.Sprite):
 					screen.blit(img,(self.rect.x+j*bricksize-camera[0],self.rect.y+i*bricksize-camera[1],bricksize,bricksize))
 		# pygame.draw.rect(screen,self.color,[self.rect.x-camera[0],self.rect.y-camera[1],self.rect.width,self.rect.height])
 
+playerdata=open('player.data','r').read()
+playerdata=playerdata.split()
+playerdata[0]=list(map(int,playerdata[0].split(',')))
 player=Player("main",(255,0,0),60,100)
-player.rect.x=20
-player.rect.y=500
+player.rect.x=playerdata[0][0]
+player.rect.y=playerdata[0][1]
 all_blocks.add(player)
 idle=pygame.image.load('character'+os.path.sep+'idle.png')
 # idle=pygame.transform.scale(idle,(player.rect.width,player.rect.height))
@@ -193,25 +197,41 @@ for i in range(5):
 	im=pygame.image.load('coins'+os.path.sep+str(i)+'.png')
 	im=pygame.transform.scale(im,(coinsize,coinsize))
 	coins.append(im)
-coin=Other("coin 1",(0,0,0),1035,400,coinsize,coins)
-all_blocks.add(coin)
-for i in range(5):
-	coin=Other("coin "+str(i+3),(0,0,0),3015+bricksize*i,950,coinsize,coins)
-	all_blocks.add(coin)
 
 diamonds=[]
 for i in range(5):
 	im=pygame.image.load('diamonds'+os.path.sep+str(i)+'.png')
 	im=pygame.transform.scale(im,(coinsize*2,coinsize*2))
 	diamonds.append(im)
-diamond=Other("diamond 0",(0,0,0),1030,300,coinsize,diamonds)
-all_blocks.add(diamond)
+
+otherdata=json.load(open('other.data','r'))
+for o in otherdata['other']:
+	obj=None
+	if o['v']:
+		if o['n']=="coin":
+			obj=Other("coin",(0,0,0),o['x'],o['y'],coinsize,coins)
+		else:
+			obj=Other("diamond",(0,0,0),o['x'],o['y'],coinsize,diamonds)
+		all_blocks.add(obj)
+# coin=Other("coin",(0,0,0),1035,400,coinsize,coins)
+# all_blocks.add(coin)
+# for i in range(5):
+# 	coin=Other("coin",(0,0,0),3015+bricksize*i,950,coinsize,coins)
+# 	all_blocks.add(coin)
+# diamond=Other("diamond",(0,0,0),1030,300,coinsize,diamonds)
+# all_blocks.add(diamond)
 
 flowerimg=pygame.image.load('flower.png')
 flowerimg=pygame.transform.scale(flowerimg,(30,30))
 flowerimgrect=flowerimg.get_rect()
-flowerimgrect.x=850
-flowerimgrect.y=90
+flowerimgrect.x=1050
+flowerimgrect.y=570
+
+cloudi=pygame.image.load('cloud.png')
+cloudi=pygame.transform.scale(cloudi,(360,120))
+cloudirect=cloudi.get_rect()
+cloudirect.x=2300
+cloudirect.y=600
 
 brickimgs=[]
 for i in range(16):
@@ -259,16 +279,31 @@ def collisions():
 			elif player.rect.y+player.rect.height>b.rect.y and player.rect.y<b.rect.y+b.rect.height:
 				if player.rect.x<b.rect.x and player.vx>0:
 					player.vx=0
-					player.rect.x=b.rect.x-player.rect.width
+					# player.rect.x=b.rect.x-player.rect.width
 				elif player.rect.x>b.rect.x and player.vx<0:
 					player.vx=0
-					player.rect.x=b.rect.x+b.rect.width
+					# player.rect.x=b.rect.x+b.rect.width
 		elif isinstance(b,Other):
-			if not b.name=="grass":
+			if b.name=="coin" or b.name=="diamond":
 				b.visible=False
 				all_blocks.remove(b)
 				del b
 				coingain.play()
+
+menu=True
+cloudirect.x=1000
+cloudirect.y=200
+while menu:
+	for e in pygame.event.get():
+		if e.type == pygame.QUIT:
+			menu=False
+	screen.fill((100,255,255))
+	cloudirect.x-=3
+	if cloudirect.x<-400:
+		cloudirect.x=1200
+	screen.blit(cloudi,cloudirect)
+	pygame.display.update()
+	clock.tick(60)
 while running:
 	for e in pygame.event.get():
 		if e.type == pygame.QUIT:
@@ -290,6 +325,8 @@ while running:
 				if colliding:
 					jumping=0
 					player.vy=-9
+			elif e.key == pygame.K_ESCAPE:
+				running=False
 		elif e.type == pygame.KEYUP:
 			if e.key == pygame.K_RIGHT:
 				pressed=False
@@ -300,7 +337,7 @@ while running:
 		player.vy+=a
 	else:
 		player.vy+=2*a
-	if not colliding and pressed:
+	if pressed and not colliding:
 		if flip:
 			player.vx=-5
 		else:
@@ -314,7 +351,9 @@ while running:
 	elif player.rect.y-camera[1]<height/4:
 		camera[1]=player.rect.y-height/4
 	collisions()
+	cloudirect.x-=1
 	screen.blit(flowerimg,(flowerimgrect.x-camera[0],flowerimgrect.y-camera[1],flowerimgrect.width,flowerimgrect.height))
+	screen.blit(cloudi,(cloudirect.x-camera[0],cloudirect.y-camera[1],cloudirect.width,cloudirect.height))
 	player.update()
 	for block in all_blocks:
 		if isinstance(block,Wall):
@@ -345,4 +384,12 @@ while running:
 	screen.blit(text,textpos)
 	pygame.display.update()
 	clock.tick(60)
-quit()
+open('player.data','w').write(str(player.rect.x)+','+str(player.rect.y))
+othr=[]
+for o in all_blocks:
+	if isinstance(o,Other):
+		d={'n':o.name,'x':o.rect.x,'y':o.rect.y,'v':o.visible}
+		othr.append(d)
+data={'cloud':[cloudirect.x,cloudirect.y],"other":othr}
+open('other.data','w').write(json.dumps(data))
+pygame.quit()
