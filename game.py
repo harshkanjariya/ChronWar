1de69ae7a8b373a7f54c36f9d07fe26d4a5175c9
harshkanjariya <harshgkanjariya2468@gmail.com>
2020-02-15 14:56:05 +0530

    menu added

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.init()
 screen=pygame.display.set_mode([1200,600])
 width,height=screen.get_size()
-running=True
+running=False
 jumping=0
 pressed=False
 flip=False
@@ -164,12 +164,7 @@ class Wall(pygame.sprite.Sprite):
 					screen.blit(img,(self.rect.x+j*bricksize-camera[0],self.rect.y+i*bricksize-camera[1],bricksize,bricksize))
 		# pygame.draw.rect(screen,self.color,[self.rect.x-camera[0],self.rect.y-camera[1],self.rect.width,self.rect.height])
 
-playerdata=open('player.data','r').read()
-playerdata=playerdata.split()
-playerdata[0]=list(map(int,playerdata[0].split(',')))
 player=Player("main",(255,0,0),60,100)
-player.rect.x=playerdata[0][0]
-player.rect.y=playerdata[0][1]
 all_blocks.add(player)
 idle=pygame.image.load('character'+os.path.sep+'idle.png')
 # idle=pygame.transform.scale(idle,(player.rect.width,player.rect.height))
@@ -204,34 +199,38 @@ for i in range(5):
 	im=pygame.transform.scale(im,(coinsize*2,coinsize*2))
 	diamonds.append(im)
 
-otherdata=json.load(open('other.data','r'))
-for o in otherdata['other']:
-	obj=None
-	if o['v']:
-		if o['n']=="coin":
-			obj=Other("coin",(0,0,0),o['x'],o['y'],coinsize,coins)
-		else:
-			obj=Other("diamond",(0,0,0),o['x'],o['y'],coinsize,diamonds)
-		all_blocks.add(obj)
-# coin=Other("coin",(0,0,0),1035,400,coinsize,coins)
-# all_blocks.add(coin)
-# for i in range(5):
-# 	coin=Other("coin",(0,0,0),3015+bricksize*i,950,coinsize,coins)
-# 	all_blocks.add(coin)
-# diamond=Other("diamond",(0,0,0),1030,300,coinsize,diamonds)
-# all_blocks.add(diamond)
+def resume_old():
+	otherdata=json.load(open('other.data','r'))
+	for o in otherdata['other']:
+		obj=None
+		if o['v']:
+			if o['n']=="coin":
+				obj=Other("coin",(0,0,0),o['x'],o['y'],coinsize,coins)
+			else:
+				obj=Other("diamond",(0,0,0),o['x'],o['y'],coinsize,diamonds)
+			all_blocks.add(obj)
+	playerdata=open('player.data','r').read()
+	playerdata=playerdata.split()
+	playerdata[0]=list(map(int,playerdata[0].split(',')))
+	player.rect.x=playerdata[0][0]
+	player.rect.y=playerdata[0][1]
+
+def newgame_load():
+	coin=Other("coin",(0,0,0),1035,400,coinsize,coins)
+	all_blocks.add(coin)
+	for i in range(5):
+		coin=Other("coin",(0,0,0),3015+bricksize*i,950,coinsize,coins)
+		all_blocks.add(coin)
+	diamond=Other("diamond",(0,0,0),1030,300,coinsize,diamonds)
+	all_blocks.add(diamond)
+	player.rect.x=10
+	player.rect.y=500
 
 flowerimg=pygame.image.load('flower.png')
 flowerimg=pygame.transform.scale(flowerimg,(30,30))
 flowerimgrect=flowerimg.get_rect()
 flowerimgrect.x=1050
 flowerimgrect.y=570
-
-cloudi=pygame.image.load('cloud.png')
-cloudi=pygame.transform.scale(cloudi,(360,120))
-cloudirect=cloudi.get_rect()
-cloudirect.x=2300
-cloudirect.y=600
 
 brickimgs=[]
 for i in range(16):
@@ -290,106 +289,165 @@ def collisions():
 				del b
 				coingain.play()
 
-menu=True
+cloudi=pygame.image.load('cloud.png')
+cloudi=pygame.transform.scale(cloudi,(360,120))
+cloudirect=cloudi.get_rect()
 cloudirect.x=1000
 cloudirect.y=200
-while menu:
-	for e in pygame.event.get():
-		if e.type == pygame.QUIT:
-			menu=False
-	screen.fill((100,255,255))
-	cloudirect.x-=3
-	if cloudirect.x<-400:
-		cloudirect.x=1200
-	screen.blit(cloudi,cloudirect)
-	pygame.display.update()
-	clock.tick(60)
-while running:
-	for e in pygame.event.get():
-		if e.type == pygame.QUIT:
-			running=False
-		if e.type == pygame.KEYDOWN:
-			if e.key == pygame.K_RIGHT:
-				pressed=True
-				player.vx=5
-				flip=False
-			elif e.key == pygame.K_LEFT:
-				pressed=True
-				flip=True
-				player.vx=-5
-			elif e.key == pygame.K_UP:
-				if colliding:
-					jumping=0
-					player.vy=-7
-			elif e.key == pygame.K_SPACE:
-				if colliding:
-					jumping=0
-					player.vy=-9
-			elif e.key == pygame.K_ESCAPE:
+mainboard=pygame.image.load('menu/menuboard.png')
+mainboard=pygame.transform.scale(mainboard,(300,400))
+mainboardrect=mainboard.get_rect()
+mainboardrect.x=width/2-150
+mainboardrect.y=height/2-200
+resumegame=pygame.image.load('menu/resume.png')
+resumegame=pygame.transform.scale(resumegame,(160,30))
+resumegamerect=mainboard.get_rect()
+resumegamerect.x=width/2-80
+resumegamerect.y=height/2-15-140
+newgame=pygame.image.load('menu/newgame.png')
+newgame=pygame.transform.scale(newgame,(200,30))
+newgamerect=mainboard.get_rect()
+newgamerect.x=width/2-100
+newgamerect.y=height/2-15-80
+options=pygame.image.load('menu/options.png')
+options=pygame.transform.scale(options,(150,30))
+optionsrect=mainboard.get_rect()
+optionsrect.x=width/2-75
+optionsrect.y=height/2-15
+exitm=pygame.image.load('menu/exit.png')
+exitm=pygame.transform.scale(exitm,(90,25))
+exitrect=mainboard.get_rect()
+exitrect.x=width/2-45
+exitrect.y=height/2-12+80
+menu=True
+def open_menu():
+	global menu,running
+	while menu:
+		for e in pygame.event.get():
+			if e.type == pygame.MOUSEBUTTONDOWN:
+				if e.button==1:
+					p=pygame.mouse.get_pos()
+					if p[0]>exitrect.x and p[1]>exitrect.y and p[0]<exitrect.width+exitrect.x and p[1]<exitrect.height+exitrect.y:
+						menu=False
+						save_game()
+						return
+					elif p[0]>newgamerect.x and p[1]>newgamerect.y and p[0]<newgamerect.width+newgamerect.x and p[1]<newgamerect.height+newgamerect.y:
+						running=True
+						newgame_load()
+						start_game()
+					elif p[0]>resumegamerect.x and p[1]>resumegamerect.y and p[0]<resumegamerect.width+newgamerect.x and p[1]<resumegamerect.height+resumegamerect.y:
+						running=True
+						resume_old()
+						start_game()
+			if e.type == pygame.QUIT:
+				menu=False
+		screen.fill((100,255,255))
+		cloudirect.x-=3
+		if cloudirect.x<-400:
+			cloudirect.x=1200
+		screen.blit(cloudi,cloudirect)
+		screen.blit(mainboard,mainboardrect)
+		screen.blit(newgame,newgamerect)
+		screen.blit(options,optionsrect)
+		screen.blit(exitm,exitrect)
+		screen.blit(resumegame,resumegamerect)
+		pygame.display.update()
+		clock.tick(60)
+def start_game():
+	global running,pressed,colliding,player,flip,jumping
+	while running:
+		for e in pygame.event.get():
+			if e.type == pygame.QUIT:
 				running=False
-		elif e.type == pygame.KEYUP:
-			if e.key == pygame.K_RIGHT:
-				pressed=False
-			elif e.key == pygame.K_LEFT:
-				pressed=False
-	screen.fill((100,255,255))
-	if player.vy<0:
-		player.vy+=a
-	else:
-		player.vy+=2*a
-	if pressed and not colliding:
-		if flip:
-			player.vx=-5
+				open_menu()
+			if e.type == pygame.KEYDOWN:
+				if e.key == pygame.K_RIGHT:
+					pressed=True
+					player.vx=5
+					flip=False
+				elif e.key == pygame.K_LEFT:
+					pressed=True
+					flip=True
+					player.vx=-5
+				elif e.key == pygame.K_UP:
+					if colliding:
+						jumping=0
+						player.vy=-7
+				elif e.key == pygame.K_SPACE:
+					if colliding:
+						jumping=0
+						player.vy=-9
+				elif e.key == pygame.K_ESCAPE:
+					running=False
+					open_menu()
+			elif e.type == pygame.KEYUP:
+				if e.key == pygame.K_RIGHT:
+					pressed=False
+				elif e.key == pygame.K_LEFT:
+					pressed=False
+		screen.fill((100,255,255))
+		if player.vy<0:
+			player.vy+=a
 		else:
-			player.vx=5
-	if player.rect.x-camera[0]<width/4:
-		camera[0]=player.rect.x-width/4
-	elif player.rect.x-camera[0]>width/2:
-		camera[0]=player.rect.x-width/2
-	if player.rect.y+player.rect.height-camera[1]>height*3/4:
-		camera[1]=player.rect.y+player.rect.height-height*3/4
-	elif player.rect.y-camera[1]<height/4:
-		camera[1]=player.rect.y-height/4
-	collisions()
-	cloudirect.x-=1
-	screen.blit(flowerimg,(flowerimgrect.x-camera[0],flowerimgrect.y-camera[1],flowerimgrect.width,flowerimgrect.height))
-	screen.blit(cloudi,(cloudirect.x-camera[0],cloudirect.y-camera[1],cloudirect.width,cloudirect.height))
-	player.update()
-	for block in all_blocks:
-		if isinstance(block,Wall):
-			block.show()
-		elif isinstance(block,Other):
-			block.update()
-			block.show()
-	if not colliding:
-		jumping=jumping+1
-		if jumping<10:
-			screen.blit(face(jump[0]),playerpos())
-		elif jumping<20:
-			screen.blit(face(jump[1]),playerpos())
-		elif jumping<30 or player.vy<0:
-			screen.blit(face(jump[2]),playerpos())
-		elif player.vy<1:
-			screen.blit(face(jump[3]),playerpos())
+			player.vy+=2*a
+		if pressed and not colliding:
+			if flip:
+				player.vx=-5
+			else:
+				player.vx=5
+		if player.rect.x-camera[0]<width/4:
+			camera[0]=player.rect.x-width/4
+		elif player.rect.x-camera[0]>width/2:
+			camera[0]=player.rect.x-width/2
+		if player.rect.y+player.rect.height-camera[1]>height*3/4:
+			camera[1]=player.rect.y+player.rect.height-height*3/4
+		elif player.rect.y-camera[1]<height/4:
+			camera[1]=player.rect.y-height/4
+		collisions()
+		cloudirect.x-=1
+		screen.blit(flowerimg,(flowerimgrect.x-camera[0],flowerimgrect.y-camera[1],flowerimgrect.width,flowerimgrect.height))
+		screen.blit(cloudi,(cloudirect.x-camera[0],cloudirect.y-camera[1],cloudirect.width,cloudirect.height))
+		player.update()
+		for block in all_blocks:
+			if isinstance(block,Wall):
+				block.show()
+			elif isinstance(block,Other):
+				block.update()
+				block.show()
+		if not colliding:
+			jumping=jumping+1
+			if jumping<10:
+				screen.blit(face(jump[0]),playerpos())
+			elif jumping<20:
+				screen.blit(face(jump[1]),playerpos())
+			elif jumping<30 or player.vy<0:
+				screen.blit(face(jump[2]),playerpos())
+			elif player.vy<1:
+				screen.blit(face(jump[3]),playerpos())
+			else:
+				screen.blit(face(jump[5]),playerpos())
+		elif colliding and player.vx==0:
+			screen.blit(face(idle),playerpos())
+			imgcount=0
 		else:
-			screen.blit(face(jump[5]),playerpos())
-	elif colliding and player.vx==0:
-		screen.blit(face(idle),playerpos())
-		imgcount=0
-	else:
-		imgcount=(imgcount+1.5)%60
-		screen.blit(face(boy[int(imgcount/6)]),playerpos())
-	text=font.render('Date and Time : '+datetime.utcnow().strftime("%d/%m/%Y %H:%M:%S"),1,(0,0,0))
-	textpos=text.get_rect(centerx=screen.get_width()/2)
-	screen.blit(text,textpos)
-	pygame.display.update()
-	clock.tick(60)
-open('player.data','w').write(str(player.rect.x)+','+str(player.rect.y))
-othr=[]
-for o in all_blocks:
-	if isinstance(o,Other):
-		d={'n':o.name,'x':o.rect.x,'y':o.rect.y,'v':o.visible}
-		othr.append(d)
-data={'cloud':[cloudirect.x,cloudirect.y],"other":othr}
-open('other.data','w').write(json.dumps(data))
+			imgcount=(imgcount+1.5)%60
+			screen.blit(face(boy[int(imgcount/6)]),playerpos())
+		text=font.render('Date and Time : '+datetime.utcnow().strftime("%d/%m/%Y %H:%M:%S"),1,(0,0,0))
+		textpos=text.get_rect(centerx=screen.get_width()/2)
+		screen.blit(text,textpos)
+		pygame.display.update()
+		clock.tick(60)
+	save_game()
+def save_game():
+	global player,all_blocks
+	open('player.data','w').write(str(player.rect.x)+','+str(player.rect.y))
+	othr=[]
+	for o in all_blocks:
+		if isinstance(o,Other):
+			d={'n':o.name,'x':o.rect.x,'y':o.rect.y,'v':o.visible}
+			othr.append(d)
+	data={'cloud':[cloudirect.x,cloudirect.y],"other":othr}
+	open('other.data','w').write(json.dumps(data))
+open_menu()
 pygame.quit()
+quit()
